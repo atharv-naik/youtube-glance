ee3f9f5b5058612a849332398b8efb2ea374daca
atharv-naik <atharvn317@gmail.com>
2024-04-15 10:21:34 +0530

    fix popup comments button size issue

--- popup-comments.js
+++ popup-comments.js
@@ -30,7 +30,7 @@ function initializeCommentsPopupButton() {
 
   commentsPopupButton.className = "ytp-button comments-popup-button";
   commentsPopupButton.innerHTML = `
-    <svg xmlns="http://www.w3.org/2000/svg" height="36px" viewBox="0 0 36 36" width="36px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M21.99 4c0-1.1-.89-2-1.99-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14l4 4-.01-18zM20 4v13.17L18.83 16H4V4h16zM6 12h12v2H6zm0-3h12v2H6zm0-3h12v2H6z"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28" fill="#FFFFFF" style="padding:1.4rem 1rem; height:3.3vh;"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M21.99 4c0-1.1-.89-2-1.99-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14l4 4-.01-18zM20 4v13.17L18.83 16H4V4h16zM6 12h12v2H6zm0-3h12v2H6zm0-3h12v2H6z"/></svg>
     `;
 
   // add other attributes
